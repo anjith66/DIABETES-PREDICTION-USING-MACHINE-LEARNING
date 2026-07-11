ee6786f0a2103372d5b922336edda706a9d598b0
Anjith K j <159188000+anjith66@users.noreply.github.com>
2026-07-11 15:22:00 +0530

    Update model loading mechanism in app
    Refactor model loading to use dynamic path for model file.

--- Diabetes Prediction Web app.py
+++ Diabetes Prediction Web app.py
@@ -4,13 +4,19 @@ Created on Fri Jul 10 19:19:42 2026
 
 @author: anjith
 """
-
+import os
 import numpy as np 
 import pickle 
 import streamlit as st 
 import pandas as pd
 import base64
-loaded_model=pickle.load(open("DIABETES_PREDICTION_.sav","rb"))
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# 2. Join the directory path with your model file name (Fix the name if needed!)
+model_path = os.path.join(BASE_DIR, "DIABETES_PREDICTION.sav")
+
+loaded_model=pickle.load(open(model_path,"rb"))
 scaler = pickle.load(open("scaler", "rb"))
 
 

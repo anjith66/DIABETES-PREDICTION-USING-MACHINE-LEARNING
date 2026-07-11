7340a298542714d0d9b4e206d20d4469d78f4733
sanshmi05 <sanshmi05@gmail.com>
2026-07-11 15:12:57 +0530

    first commit

--- Diabetes Prediction Web app.py
+++ Diabetes Prediction Web app.py
@@ -10,7 +10,7 @@ import pickle
 import streamlit as st 
 import pandas as pd
 import base64
-loaded_model=pickle.load(open("D:/projects ml/DIABITIES-PREDICTION/DIABETES_PREDICTION_.sav","rb"))
+loaded_model=pickle.load(open("D:/projects ml/DIABITIES-PREDICTION/DIABETES_PREDICTION.sav","rb"))
 scaler = pickle.load(open("D:/projects ml/DIABITIES-PREDICTION/scaler.sav", "rb"))
 
 
--- diabetes_predictive_system.py
+++ diabetes_predictive_system.py
@@ -5,26 +5,31 @@ Created on Fri Jul 10 19:13:56 2026
 @author: anjith
 """
 
-import pickle 
-import numpy as np 
-loaded_model=pickle.load(open("D:/projects ml/DIABITIES-PREDICTION/DIABETES_PREDICTION_.sav","rb"))
+import pickle
+import numpy as np
+import pandas as pd
+
+loaded_model=pickle.load(open("D:/projects ml/DIABITIES-PREDICTION/DIABETES_PREDICTION.sav","rb"))
 scaler = pickle.load(open("D:/projects ml/DIABITIES-PREDICTION/scaler.sav", "rb"))
 
 
 input_data = (5,166,72,19,175,25.8,0.587,51)
 
-# changing the input_data to numpy array
+# 2. Change it to a numpy array and reshape
 input_data_as_numpy_array = np.asarray(input_data)
+input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
 
-# reshape the array as we are predicting for one instance
-input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+# 3. Create a DataFrame with the exact feature names your scaler expects
+# (Replace these with the exact names used during your model training)
+feature_names = ['Pregnancies', 'Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI', 'DiabetesPedigreeFunction', 'Age']
+input_data_df = pd.DataFrame(input_data_reshaped, columns=feature_names)
 
-# standardize the input data
-std_data = scaler.transform(input_data_reshaped)
-print(std_data)
+# 4. Standardize the data using the DataFrame instead of the raw array
+std_data = scaler.transform(input_data_df)
 
+# 5. Predict
 prediction = loaded_model.predict(std_data)
-print(prediction)
+print("prediction:",prediction[0])
 
 if (prediction[0] == 0):
   print('The person is not diabetic')
